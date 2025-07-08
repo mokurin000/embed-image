1c51190822a547c2e7784e035c134d740a763a30
mokurin000 <1348292515a@gmail.com>
2025-07-08 19:19:02 +0800

    feat: qrcode overlapping & logging

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use std::{
+    borrow::Cow,
     error::Error,
     fs::{self, OpenOptions},
-    io::Write,
+    io::{BufReader, Write},
+    ops::Div,
     path::{Path, PathBuf},
 };
 
+use image::{EncodableLayout, ImageEncoder as _, codecs::png::PngEncoder, imageops::overlay};
+use qrencode::{EcLevel, QrCode};
+use spdlog::{error, info, warn};
 use zip::write::FileOptions;
 
 #[derive(Debug, palc::Parser)]
 pub struct Args {
     /// specify a password, optional
-    #[arg(short = 'p')]
+    #[arg(long, short = 'p')]
     password: Option<String>,
 
-    /// whether to add an QRCode overlap for password
-    #[arg(short = 'S')]
+    /// add an QRCode overlap for password
+    #[arg(long, short = 'Q')]
     qrcode_overlap: bool,
 
+    /// remove quiet zone of QR Code
+    ///
+    /// Quiet zone means the surrounding blank area
+    #[arg(long, short = 'q')]
+    remove_quiet_zone: bool,
+
+    /// Position of QR code. Can be one of `top-left` (default), `top-right`, `bottom-left`, `bottom-right`
+    ///
+    /// will fallback to default on invalid input.
+    #[arg(long, short = 'P')]
+    qr_position: Option<String>,
+
+    /// target file. if enabled `qrcode_overlap, must be one of PNG, JPEG and WEBP.`
     img: PathBuf,
     path: Vec<PathBuf>,
 }
@@ -26,8 +44,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         img,
         path,
         password,
-        qrcode_overlap: _todo,
+        qr_position,
+        qrcode_overlap,
+        remove_quiet_zone,
     } = palc::Parser::parse();
+
+    if !img.exists() {
+        error!("input image not existing!");
+    }
 
     let output_fn = img
         .file_name()
@@ -38,8 +62,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap()
         .to_string()
         + "_merged."
-        + &img.extension().unwrap().to_string_lossy();
-    let img_data = fs::read(img)?;
+        + &if qrcode_overlap {
+            Cow::from("png")
+        } else {
+            img.extension().unwrap().to_string_lossy()
+        };
 
     let mut output = OpenOptions::new()
         .append(false)
@@ -48,7 +75,67 @@ fn main() -> Result<(), Box<dyn Error>> {
         .read(false)
         .open(&output_fn)?;
 
-    output.write_all(&img_data)?;
+    info!("reading source image");
+
+    if let Some(pass) = password.as_deref()
+        && qrcode_overlap
+    {
+        let file = OpenOptions::new().read(true).create(false).open(img)?;
+        let bufreader = BufReader::new(file);
+
+        info!("start pixel converting");
+
+        let mut orig_image = image::io::Reader::new(bufreader)
+            .with_guessed_format()?
+            .decode()?
+            .to_rgba16();
+
+        info!("start QR Code generation");
+
+        let orig_width = orig_image.width();
+        let orig_height = orig_image.height();
+        let pixel_len = orig_width.min(orig_height).div(3).max(200);
+        let qrcode_img = QrCode::with_error_correction_level(pass, EcLevel::H)?
+            .render::<image::Rgba<u16>>()
+            .max_dimensions(pixel_len, pixel_len)
+            .quiet_zone(!remove_quiet_zone)
+            .build();
+
+        let (x, y) = match qr_position.as_deref() {
+            Some("top-right") => (orig_width - pixel_len, 0),
+            Some("bottom-left") => (0, orig_height - pixel_len),
+            Some("bottom-right") => (orig_width - pixel_len, orig_height - pixel_len),
+            Some(pos) => {
+                if pos != "top-left" {
+                    warn!("unknown position {pos}, falling back to top-left");
+                }
+                (0, 0)
+            }
+            _ => (0, 0),
+        };
+
+        info!("overlapping QR Code on original image");
+        overlay(&mut orig_image, &qrcode_img, x.into(), y.into());
+
+        info!("writing overlapped image");
+        let encoder = PngEncoder::new(&mut output);
+        encoder.write_image(
+            orig_image.as_bytes(),
+            orig_image.width(),
+            orig_image.height(),
+            image::ColorType::Rgba16,
+        )?;
+    } else {
+        if qrcode_overlap {
+            warn!("QR Code overlap does nothing if did not specify a password");
+        }
+
+        info!("copying original image");
+        let img_data = fs::read(img)?;
+        output.write_all(&img_data)?;
+    }
+
+    info!("writing ZIP, password: {password:?}");
 
     let mut writer = zip::ZipWriter::new(output);
     let mut options: FileOptions<'_, ()> = FileOptions::default()
@@ -68,10 +155,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for path in path_to_pack {
         writer.start_file_from_path(&path, options)?;
-        writer.write_all(&fs::read(&path)?)?;
+        let data = fs::read(&path)?;
+        let size = humansize::format_size(data.len(), humansize::BINARY);
+        info!("read {} of {size}, compressing...", path.to_string_lossy(),);
+        writer.write_all(&data)?;
     }
 
     writer.finish()?;
+
+    info!("all tasks finished without any error.");
 
     Ok(())
 }
